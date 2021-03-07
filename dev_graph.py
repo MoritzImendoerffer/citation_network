6c62d57d02a56e3f0ec5f411e8cc9dfba84dea5f
MoritzImendoerffer <moritz.imend@gmail.com>
2021-03-07 20:42:14 +0100

    Funktioniert. TODO: 1) Liste mit papers EXTEND liste mit related 2) `get_citations` und `fill` über alle einträge aus 1. alles operiert nur auf Liste aus 1

--- dev_graph.py
+++ dev_graph.py
@@ -1,14 +1,15 @@
-from scholar_classes import MyScholarlyScraper
+from scholar_classes import MyScholarlyScraper, get_cited_by
 from scholarly import scholarly
 import networkx as nx
 import random
 import time
 import pickle
 import datetime
+import concurrent
 
 sc = MyScholarlyScraper(search_term='"digital twin" AND "biotechnology"')
 # sc = MyScholarlyScraper(search_term='biomass valorization')
-sc.load_n_items(10)
+sc.load_n_items(20)
 
 def subsequent_iterator(a=0, n=1e3):
     n = int(n)
@@ -19,68 +20,57 @@ def get_hash(scholarly_item):
     return hash(scholarly_item['bib']['title'].replace(' ', ''))
 
 g = nx.Graph()
-success = 0
-fail = 0
 
 for i, active_item in enumerate(sc.active_items):
     print(100 * '=')
     print('Started NX Generation')
     print(100 * '=')
-    active_item_id = get_hash(active_item)
+    active_item_id = get_hash(active_item[1])
     g.add_nodes_from([(active_item_id, {'meta': active_item})])
 
-    # the related and citing articles for the current active article
+    # the related and citing articles for the current active article (since they are already sorted, simple indexing
+    # is enough). Looks like [(index, [{}, {}, ...], success message), ...]
     related_items = sc.related[i]
     citing_items = sc.cited_by[i]
-    if citing_items:
-        for citing_item in citing_items:
+    if citing_items[2]:
+        for citing_item in citing_items[1]:
             # node id for the current related article
             citing_item_id = get_hash(citing_item)
             g.add_nodes_from([(citing_item_id, {'meta': citing_item})])
             g.add_edge(active_item_id, citing_item_id)
 
     # get the citing artcicles of the related items
-    if related_items:
+    if related_items[2]:
         print(100 * '-')
         print(f'Started iteration through related items')
         print(100 * '-')
-        for related_item in related_items:
-            # node id for the current related article
-            related_item_id = get_hash(related_item)
-            g.add_nodes_from([(related_item_id, {'meta': related_item})])
-            try:
-                related_cited_parser = scholarly.citedby(related_item)
-                print(100*'-')
-                print('Related Articles: Fetch SUCESS')
-                print(100 * '-')
-                success += 1
-            except Exception as e:
-                try:
-                    w = random.uniform(1, 2)
-                    time.sleep(w)
-                    print(f'{i}: Exception: {e} for item {related_item}, retrying')
-                    related_cited_parser = scholarly.citedby(related_item)
-                    success += 1
-                except Exception as e:
-                    print(f'{i}: Exception: {e} for item {related_item}')
-                    print(100 * '-')
-                    print(f'Related Articles: Fetch Failed: {e}')
-                    print(100 * '-')
-                    fail += 1
-                    continue
+        if related_items[1]:
+            for related_item in related_items[1]:
+                # node id for the current related article
+                related_item_id = get_hash(related_item)
+                g.add_nodes_from([(related_item_id, {'meta': related_item})])
 
-            related_citing_items = list(related_cited_parser)
-            for related_citing_item in related_citing_items:
-                # node id for the current citing article
-                related_citing_item_id = get_hash(related_citing_item)
-                g.add_nodes_from([(related_citing_item_id, {'meta': related_citing_item})])
-                g.add_edge(related_item_id, related_citing_item_id)
+            with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+                # a bit hacky but I have to add the index (related_item[0]) to the call, although it is the same for
+                # all items
+                related_citations_list = list(executor.map(lambda item: get_cited_by((related_items[0], item)), related_items[1]))
+
+            # related citations list looks like (index, [{}, {}, ...], success)
+            for related_citing_item in related_citations_list:
+                if related_citing_item[2]:  # success
+                    # node id for the current citing article
+                    if related_citing_item[1]:  # list could be empty
+                        for rel_item in related_citing_item[1]:
+                            related_citing_item_id = get_hash(rel_item)
+                            g.add_nodes_from([(related_citing_item_id, {'meta': related_citing_item})])
+                            g.add_edge(related_item_id, related_citing_item_id)
+
 print(100 * '=')
 print('Stopped NX Generation')
 print(100 * '=')
 
 from matplotlib import pyplot as plt
-fig, ax = plt.subplots(ncols=1, figsize=(10, 10), dpi=300)
+fig, ax = plt.subplots(ncols=1, figsize=(20, 20), dpi=300)
 pos = nx.spring_layout(g)
 nx.draw(g, pos=pos, with_labels=False, node_size=0.2, width=0.1, ax=ax)
 plt.show()
--- scholar_classes.py
+++ scholar_classes.py
@@ -1,23 +1,25 @@
 from scholarly import scholarly
+import concurrent.futures
 from include.scraper_api_proxy import ScraperAPI as ProxyGenerator
 # from scholarly import ProxyGenerator
 # import logging
 # logging.basicConfig(level=logging.DEBUG)
 
 class MyScholarlyScraper:
-    def __init__(self, search_term: str):
+    def __init__(self, search_term: str, n_threads=10):
         self.search_term = search_term
         self.search_result = None
         self.active_items = []
         self.cited_by = []
         self.citations = []
         self.related = []
+        self.n_threads = n_threads
 
     def search(self):
         res = scholarly.search_pubs(self.search_term)
         self.search_result = res
 
-    def load_n_items(self, n=10, fill=False, citations=True, related=True):
+    def load_n_items(self, n=10, fill=True, citations=True, related=True):
         print(100 * '=')
         print('Started Load')
         print(100 * '=')
@@ -32,47 +34,101 @@ class MyScholarlyScraper:
                 print(f'load_n_items: load FAILED: {e}')
                 print(100 * '-')
 
-        for i in range(n):
-            item = next(self.search_result)
-            if fill:
-                try:
-                    item = scholarly.fill(item)
-                    print(100 * '-')
-                    print(f'load_n_items: fill SUCCESS')
-                    print(100 * '-')
-                except Exception as e:
-                    print(100 * '-')
-                    print(f'load_n_items: fill FAILED: {e}')
-                    print(100 * '-')
-            self.active_items.append(item)
+        # exhaust n items
+        search_items = [next(self.search_result) for i in range(n)]
 
-            if citations:
-                try:
-                    cited_parser = scholarly.citedby(item)
-                    self.cited_by.append(list(cited_parser))
-                    print(100 * '-')
-                    print(f'load_n_items: citations SUCCESS')
-                    print(100 * '-')
-                except Exception as e:
-                    print(100 * '-')
-                    print(f'load_n_items: citations FAILED: {e}')
-                    print(100 * '-')
-                    self.cited_by.append(None)
+        # label the items to sort all results after threading using a consecutive index
+        if self.active_items:
+            start_index = min([item[0]] for item in self.active_items)[0]
+        else:
+            start_index = 0
+        ind = subsequent_iterator(start_index, 1e8)
 
-            if related:
-                try:
-                    related_parser = scholarly.get_related_articles(item)
-                    self.related.append(list(related_parser))
-                    print(100 * '-')
-                    print(f'load_n_items: related SUCCESS')
-                    print(100 * '-')
-                except Exception as e:
-                    print(100 * '-')
-                    print(f'load_n_items: related FAILED: {e}')
-                    print(100 * '-')
-                    self.related.append(None)
-        print(100*'=')
-        print('Stopped Load')
-        print(100 * '=')
-    def citations(self):
-        print('Not known yet, how to do that')
+        # search items always should look like (index, item, ...)
+        search_items = [(next(ind), item) for item in search_items]
+
+        if fill:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=self.n_threads) as executor:
+                search_items_filled = list(executor.map(lambda item: get_filled(item), search_items))
+
+            self.active_items.extend(search_items_filled)
+        else:
+            self.active_items.extend(search_items)
+
+        if citations:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=self.n_threads) as executor:
+                cited_by = list(executor.map(lambda item: get_cited_by(item), search_items))
+            self.cited_by.extend(cited_by)
+
+        if related:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=self.n_threads) as executor:
+                related_list = list(executor.map(lambda item: get_related(item), search_items))
+            self.related.extend(related_list)
+
+        self.active_items.sort(key=lambda x: x[0])
+        self.cited_by.sort(key=lambda x: x[0])
+        self.related.sort(key=lambda x: x[0])
+
+def subsequent_iterator(a=0, n=1e3):
+    n = int(n)
+    for i in range(a, a+n):
+        yield i
+
+def get_related(item):
+    print(100 * '-')
+    print(f'get_related: Started')
+    print(100 * '-')
+    success = False
+
+    index, _item = item
+    try:
+        related_parser = scholarly.get_related_articles(_item)
+        related_list = list(related_parser)
+        success = True
+    except Exception as e:
+        print(f'get_related: Exception: {e}')
+        related_list = None
+
+    print(100 * '-')
+    print(f'get_related: Stopped, Success = {success}')
+    print(100 * '-')
+    return (index, related_list, success)
+
+def get_cited_by(item):
+    print(100 * '-')
+    print(f'get_citations: Started')
+    print(100 * '-')
+    success = False
+
+    index, _item = item
+    try:
+        cited_parser = scholarly.citedby(_item)
+        cited_list = list(cited_parser)
+        success = True
+    except Exception as e:
+        print(f'get_cited_by: Exception: {e}')
+        cited_list = None
+
+    print(100 * '-')
+    print(f'get_citations: Stopped, Success = {success}')
+    print(100 * '-')
+    return (index, cited_list, success)
+
+def get_filled(item):
+    print(100 * '-')
+    print(f'fill: Started')
+    print(100 * '-')
+    success = False
+
+    index, _item = item
+    try:
+        _item = scholarly.fill(_item)
+        success = True
+    except Exception as e:
+        print(f'get_filled: Exception: {e}')
+        pass
+
+    print(100 * '-')
+    print(f'fill: Stopped, Success = {success}')
+    print(100 * '-')
+    return (index, _item, success)
